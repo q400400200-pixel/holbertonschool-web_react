1a082793bfcdbe4e6f8a7e74f1cc6a1d4e406680
q400400200-pixel <q.400400200@gmail.com>
2026-04-18 22:26:40 +0300

    Update bundle.js

--- TypeScript/task_0/dist/bundle.js
+++ TypeScript/task_0/dist/bundle.js
@@ -1,1 +1,38 @@
-h
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/*!********************!*\
+  !*** ./js/main.ts ***!
+  \********************/
+
+var student1 = {
+    firstName: "Abdulrahman",
+    lastName: "Dhaifallah",
+    age: 26,
+    location: "Riyadh"
+};
+var student2 = {
+    firstName: "Dhaifallah",
+    lastName: "Abdulrahman",
+    age: 62,
+    location: "Jeddah"
+};
+var studentsList = [student1, student2];
+var table = document.createElement("table");
+var tbody = document.createElement("tbody");
+studentsList.forEach(function (student) {
+    var row = document.createElement("tr");
+    var firstNameCell = document.createElement("td");
+    var locationCell = document.createElement("td");
+    firstNameCell.textContent = student.firstName;
+    locationCell.textContent = student.location;
+    row.appendChild(firstNameCell);
+    row.appendChild(locationCell);
+    tbody.appendChild(row);
+});
+table.appendChild(tbody);
+document.body.appendChild(table);
+if (document.body) {
+    document.body.appendChild(table);
+}
+/******/ })()
+;
