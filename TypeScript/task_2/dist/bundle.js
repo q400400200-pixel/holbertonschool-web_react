7f6cdea339d5630a91e800d9595cbbfd0c444357
q400400200-pixel <q.400400200@gmail.com>
2026-04-18 22:07:35 +0300

    Create bundle.js

---
+++ TypeScript/task_2/dist/bundle.js
@@ -0,0 +1,67 @@
+ /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/*!********************!*\
+  !*** ./js/main.ts ***!
+  \********************/
+
+var Director = /** @class */ (function () {
+    function Director() {
+    }
+    Director.prototype.workFromHome = function () {
+        return "Working from home";
+    };
+    Director.prototype.getCoffeeBreak = function () {
+        return "Getting a coffee break";
+    };
+    Director.prototype.workDirectorTasks = function () {
+        return "Getting to director tasks";
+    };
+    return Director;
+}());
+var Teacher = /** @class */ (function () {
+    function Teacher() {
+    }
+    Teacher.prototype.workFromHome = function () {
+        return "Cannot work from home";
+    };
+    Teacher.prototype.getCoffeeBreak = function () {
+        return "Cannot have a break";
+    };
+    Teacher.prototype.workTeacherTasks = function () {
+        return "Getting to work";
+    };
+    return Teacher;
+}());
+function createEmployee(salary) {
+    if (typeof salary === "number" && salary < 500) {
+        return new Teacher();
+    }
+    return new Director();
+}
+console.log(createEmployee(200));
+console.log(createEmployee(1000));
+console.log(createEmployee('$500'));
+function isDirector(employee) {
+    return employee instanceof Director;
+}
+function executeWork(employee) {
+    if (isDirector(employee)) {
+        console.log(employee.workDirectorTasks());
+    }
+    else {
+        console.log(employee.workTeacherTasks());
+    }
+}
+executeWork(createEmployee(200));
+executeWork(createEmployee(1000));
+function teachClass(todayClass) {
+    if (todayClass === "Math") {
+        return "Teaching Math";
+    }
+    return "Teaching English";
+}
+console.log(teachClass("Math"));
+console.log(teachClass("English"));
+
+/******/ })()
+;
